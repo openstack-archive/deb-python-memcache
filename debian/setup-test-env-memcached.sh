a7319f26a277b99b8f6d26a11bb52ff1185e9e90
Thomas Goirand <thomas@goirand.fr>
2016-02-05 08:44:02 +0000

    Cleans setup-test-env-memcached.sh from unused function wait_for_line.

--- debian/setup-test-env-memcached.sh
+++ debian/setup-test-env-memcached.sh
@@ -8,14 +8,6 @@ clean_exit () {
 	sleep 1
 }
 
-wait_for_line () {
-	while read line ; do
-		echo "$line" | grep -q "$1" && break
-	done < "$2"
-	# Read the fifo for ever otherwise process would block
-	cat "$2" >/dev/null &
-}
-
 trap clean_exit EXIT
 /usr/bin/memcached -m 64 -p 11212 -u memcache -l 127.0.0.1 & PID_OF_MEMCACHE=$!
 sleep 2
